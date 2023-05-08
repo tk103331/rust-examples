c262194328333c046585261fcd45edb631b4a3a4
tk103331 <tk103331@gmail.com>
2023-05-08 12:27:34 +0800

    freya hello_world

---
+++ freya/hello_world/src/main.rs
@@ -0,0 +1,29 @@
+#![cfg_attr(
+    all(not(debug_assertions), target_os = "windows"),
+    windows_subsystem = "windows"
+)]
+
+use freya::prelude::*;
+
+fn main() {
+    launch(app);
+}
+
+fn app(cx: Scope) -> Element {
+    let mut count = use_state(cx, || 0);
+
+    render!(
+        container {
+            height: "100%",
+            width: "100%",
+            background: "rgb(35, 35, 35)",
+            color: "white",
+            padding: "12",
+            Button {
+                onclick: move |_| count += 1,
+                label { "Click to decrease  " }
+            },
+            label { " -> {count}" }
+        }
+    )
+}
